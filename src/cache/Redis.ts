80bbf851700cb570a74abdc2ad8876e0a330aef6
Sumit Kumar <itsme.sumit96@gmail.com>
2025-03-17 17:05:09 +0530

    Lint fixes for unused imports

--- src/cache/Redis.ts
+++ src/cache/Redis.ts
@@ -1,4 +1,4 @@
-import Redis, { RedisOptions } from 'ioredis';
+import Redis from 'ioredis';
 import Valkey from 'iovalkey';
 
 class Client {
@@ -8,9 +8,11 @@ class Client {
     try {
       if (!Client.instance) {
         const buildUrl = `${process.env.CACHE_SECURE === 'true' ? 'rediss' : 'redis'}://${
-          process.env.REDIS_USERNAME ? `${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@` : ''
+          process.env.REDIS_USERNAME
+            ? `${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@`
+            : ''
         }${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
-        
+
         if (process.env.CACHE_DRIVER === 'redis') {
           Client.instance = new Redis(buildUrl);
         } else if (process.env.CACHE_DRIVER === 'valkey') {
@@ -18,19 +20,19 @@ class Client {
         } else {
           throw new Error('Cache driver not supported');
         }
-  
+
         Client.instance.on('error', (error) => {
           console.error(`${process.env.CACHE_DRIVER} error:`, error);
         });
-  
+
         Client.instance.on('connect', () => {
           console.log(`${process.env.CACHE_DRIVER} connected`);
         });
-  
+
         Client.instance.on('reconnecting', () => {
           console.log(`${process.env.CACHE_DRIVER} reconnecting`);
         });
-  
+
         Client.instance.on('end', () => {
           console.log(`${process.env.CACHE_DRIVER} connection ended`);
         });
